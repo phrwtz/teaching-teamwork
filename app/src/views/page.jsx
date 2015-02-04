d0f36e4094a92fdb4cf6a7f208e502171c3fe057
Sam Fentress <sfentress@concord.org>
2015-02-04 09:40:40 -0500

    Add basic image handling

--- app/src/views/page.jsx
+++ app/src/views/page.jsx
@@ -1,13 +1,20 @@
 require('./chat.jsx');
 
+var config = require('../config');
+
 module.exports = PageView = React.createClass({
   render: function() {
     var title,
-        activity = this.props.activity ? this.props.activity : {}
+        activity = this.props.activity ? this.props.activity : {},
+        image = null;
     if (activity.name) {
       title = <h1>Teaching Teamwork: { activity.name }</h1>
     } else {
       title = <h1>Teaching Teamwork</h1>
+    }
+
+    if (activity.image) {
+      image = <img src={ config.modelsBase + activity.image } />
     }
     return (
       <div className="tt-page">
@@ -15,7 +22,7 @@ module.exports = PageView = React.createClass({
         <h2>Circuit { this.props.circuit }</h2>
         <div id="breadboard-wrapper"></div>
         <ChatView {...activity} />
-        <div id="image-wrapper"><img src="circuit.png" /></div>
+        <div id="image-wrapper">{ image }</div>
       </div>
     );
   }
